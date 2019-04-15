54f3313eae5fc91e7179cc5d9525de472bc55792
Brian-T-Hart <hart.briant@gmail.com>
2019-04-14 21:02:37 -0700

    added home component

---
+++ resources/js/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => (
+	<div className="section d-flex flex-column justify-content-center pl-5 pr-5" id="section-home">
+		<h1 className="portfolio-name">Brian Hart</h1>
+		<h2 className="portfolio-description">Full Stack Web Developer</h2>
+		<button className="btn btn-primary">Hijack this page</button>
+	</div>
+)
+
+export default Home;
