378665c0ae256f16d83c69f81e8791fc1848493e
Brian-T-Hart <hart.briant@gmail.com>
2019-04-16 11:32:36 -0700

    changed Home component to a functional component

--- resources/js/components/Home/Home.js
+++ resources/js/components/Home/Home.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Home = () => (
-	<div className="section d-flex flex-column justify-content-center pl-5 pr-5" id="section-home">
-		<h1 className="portfolio-name">Brian Hart</h1>
-		<h2 className="portfolio-description">Full Stack Web Developer</h2>
-		<button className="btn btn-primary">Hijack this page</button>
-	</div>
-)
+class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			portfolioName: 'Brian Hart',
+			portfolioDescription: 'Full Stack Web Developer'
+		}
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		console.log('clicked');
+	}
+
+	handleChange() {
+		console.log('changing');
+	}
+
+    render() {
+        return (
+            <div className="section d-flex flex-column justify-content-center pl-5 pr-5" id="section-home">
+				<h1 className="portfolio-name" id="portfolio-name">{this.state.portfolioName}</h1>
+				<h2 className="portfolio-description">{this.state.portfolioDescription}</h2>
+				<button className="btn btn-primary" onClick={this.handleClick}></button>
+			</div>
+        );
+    }
+}
 
 export default Home;
