f4c8111f8ccbab681605845d6ee8f03a448f3ac5
Brian-T-Hart <hart.briant@gmail.com>
2019-04-16 11:30:33 -0700

    updated html classes

--- resources/js/components/Navbar/Navbar.js
+++ resources/js/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NavIcon from '../Icons/NavIcon';
 
 const Navbar = () => (
-	<div className="d-flex flex-column justify-content-around" id="section-navbar">
+	<div className="d-sm-flex flex-sm-column justify-content-around" id="section-navbar">
 		<NavIcon className="fas fa-home" dataId="section-home" iText="Home" />
 		<NavIcon className="fas fa-user" dataId="section-about" iText="About" />
 		<NavIcon className="fas fa-newspaper" dataId="section-projects" iText="Projects" />
