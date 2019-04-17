73f853776a43a1e8d96cb9b308d353d0de8188e2
Brian-T-Hart <hart.briant@gmail.com>
2019-04-16 19:34:42 -0700

    added project details array

---
+++ resources/js/components/Projects/projectDetails.js
@@ -0,0 +1,34 @@
+const projectDetails = [
+	{
+		id: 1,
+		title: 'TutorApp',
+		subtitle: 'Laravel, PHP, React, MySQL',
+		description: 'A web application for tutors to record student info, schedule lessons, record payments, track income and quickly communicate with clients.',
+		image_link: 'https://www.brianthart.com/assets/images/lifeCalling.png',
+		web_link: 'https://www.tutorapp.app',
+		github_link: 'https://github.com/Brian-T-Hart/tutor-tool',
+		tags: ['laravel', 'php', 'react', 'mysql']
+	},
+	{
+		id: 2,
+		title: 'WCHS Worlviews',
+		subtitle: 'MySQL, Express, React, Node',
+		description: 'Thanks for checking out WCHS-Lifecalling. I had a lot of fun working on this React app that I made for a high school Life Calling class. The app allows students to record and track information regarding their personality and strengths as they take various tests throughout the year. After each test, the students select values from a list and the data is quickly populated to the screen and stored in a MySQL database for future retrieval. The app also allows for the upload of three documents in pdf, jpeg, or png format. The site was constructed using a MySQL database, Express, React on the front end, Node.js, and Cloudinary for document management.',
+		image_link: 'https://www.brianthart.com/assets/images/lifeCalling.png',
+		web_link: 'https://wchs-lifecalling-demo.herokuapp.com/',
+		github_link: 'https://github.com/Brian-T-Hart/WCHS-Lifecalling-demo',
+		tags: ['cloudinary', 'express', 'node', 'react', 'mysql']
+	},
+	{
+		id: 3,
+		title: 'ClassWorx',
+		subtitle: 'MySQL, Express, Handlebars, Node',
+		description: '',
+		image_link: 'https://www.brianthart.com/assets/images/classworx2.png',
+		web_link: '',
+		github_link: '',
+		tags: ['cloudinary', 'node', 'express', 'handlebars', 'mysql']
+	}
+];
+
+export default projectDetails;
