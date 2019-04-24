9ffb297f74373dedad907b40bea44477f3d99284
Brian-T-Hart <hart.briant@gmail.com>
2019-04-24 01:16:20 -0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Force SSL in production
+        if ($this->app->environment() == 'production') {
+            
+            URL::forceScheme('https');
+        }
     }
 }
