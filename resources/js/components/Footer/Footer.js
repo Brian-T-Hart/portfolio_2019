b5c38ac4e2f202e662d23b502259c48cb61ba9a3
Brian-T-Hart <hart.briant@gmail.com>
2019-04-24 08:34:01 -0700

    removed 'sm' from d-sm-flex

--- resources/js/components/Footer/Footer.js
+++ resources/js/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import FooterIcon from '../Icons/FooterIcons';
 
 const Footer = () => (
-	<div className="d-sm-flex justify-content-center bg-darkgray" id="footer">
+	<div className="d-flex justify-content-center bg-darkgray" id="footer">
 		<FooterIcon href="https://www.linkedin.com/in/hartbriant/" className="fab fa-linkedin-in" />
 		<FooterIcon href="https://github.com/Brian-T-Hart" className="fab fa-github" />
 		<FooterIcon href="https://stackoverflow.com/users/10062801/brian-hart?tab=profile" className="fab fa-stack-overflow" />
