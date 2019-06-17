e048bab19a1caea8dfa8bec33c3f4cc93673e54e
Brian-T-Hart <hart.briant@gmail.com>
2019-06-17 10:01:39 -0700

    added description and keywords meta tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Brian T Hart | Full Stack Web Developer">
+    <meta name="keywords" content="portfolio, website, web developer, full-stack, JavaScript, HTML, CSS, jQuery, Laravel, PHP, Git, MySQL">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
